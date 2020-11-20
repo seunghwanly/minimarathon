5d7f344ab44f34701078fdd1cb95e65a95f652bb
jonghapark <59429710+jonghapark@users.noreply.github.com>
2020-11-20 22:48:01 +0900

    location_toFinish

--- minimarathon/lib/component/body/relay/relay_finish.dart
+++ minimarathon/lib/component/body/relay/relay_finish.dart
@@ -6,12 +6,23 @@ import '../../../util/text_style.dart';
 import 'ranking.dart';
 
 class RelayFinish extends StatefulWidget {
+  final int recordTime;
+  final double totalDistance;
+
+  RelayFinish({this.recordTime, this.totalDistance});
   @override
   RelayFinishState createState() => RelayFinishState();
 }
 
 class RelayFinishState extends State<RelayFinish> {
   final String username = 'Jong Ha Park';
+
+  String _printDuration(Duration duration) {
+    String twoDigits(int n) => n.toString().padLeft(2, "0");
+    String twoDigitMinutes = twoDigits(duration.inMinutes.remainder(60));
+    String twoDigitSeconds = twoDigits(duration.inSeconds.remainder(60));
+    return "${twoDigits(duration.inHours)}:$twoDigitMinutes:$twoDigitSeconds";
+  }
 
   void _navigationMore() {
     Navigator.pop(context);
@@ -40,7 +51,7 @@ class RelayFinishState extends State<RelayFinish> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Expanded(
-                flex: 4,
+                flex: 3,
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.end,
                   children: [
@@ -51,7 +62,7 @@ class RelayFinishState extends State<RelayFinish> {
               ),
               Expanded(flex: 1, child: Container(child: Text(''))),
               Expanded(
-                flex: 4,
+                flex: 5,
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
@@ -67,8 +78,13 @@ class RelayFinishState extends State<RelayFinish> {
                         child: Column(children: [
                           makeTextThin("Congratulations,", Colors.white, 20),
                           makeTextThin("Jong ha Park" + '!', Colors.white, 20),
-                          makeTwoColor("You walked ", "3.2km !", Colors.white,
+                          makeTwoColor("You walked ", "4.0km !", Colors.white,
                               Colors.white, 20),
+                          makeTextSemiThin(
+                              _printDuration(
+                                  Duration(seconds: widget.recordTime)),
+                              Colors.white,
+                              20),
                         ]))
                   ],
                 ),
