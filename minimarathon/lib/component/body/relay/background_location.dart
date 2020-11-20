e2b14d4a4e04ab92741f6489a63976f145becf25
jonghapark <59429710+jonghapark@users.noreply.github.com>
2020-11-20 22:19:05 +0900

    Time Recode

--- minimarathon/lib/component/body/relay/background_location.dart
+++ minimarathon/lib/component/body/relay/background_location.dart
@@ -1,8 +1,13 @@
 import 'package:background_location/background_location.dart';
 import 'package:flutter/material.dart';
 import 'dart:math';
+import 'dart:async';
+import 'package:intl/intl.dart';
 
 import 'package:minimarathon/component/header/header.dart';
+
+//util
+import '../../../util/palette.dart';
 
 class MyBackgroundLocation extends StatefulWidget {
   @override
@@ -25,6 +30,37 @@ class MyBackgroundLocationState extends State<MyBackgroundLocation> {
 
   double totalDistance = 0;
   double totalDistance2 = 0;
+  var date = new DateTime(1996, 5, 23, 0, 0, 0);
+  Timer _timer;
+  int _start = 0;
+  bool isStart = false;
+
+  String _printDuration(Duration duration) {
+    String twoDigits(int n) => n.toString().padLeft(2, "0");
+    String twoDigitMinutes = twoDigits(duration.inMinutes.remainder(60));
+    String twoDigitSeconds = twoDigits(duration.inSeconds.remainder(60));
+    return "${twoDigits(duration.inHours)}:$twoDigitMinutes:$twoDigitSeconds";
+  }
+
+  void startTimer() {
+    if (isStart == true) return;
+    const oneSec = const Duration(seconds: 1);
+    _timer = new Timer.periodic(
+      oneSec,
+      (Timer timer) => setState(
+        () {
+          if (isStart == false) isStart = true;
+          _start = _start + 1;
+
+          // date = (new DateTime(1996,5,23, _start~/3600 ,
+          //  (_start-(_start~/3600)*3600) ~/ 60 ,
+          //  ((_start-(_start~/3600)*3600) ~/ 60 ) * 60
+          //  );
+        },
+      ),
+    );
+  }
+
   @override
   void initState() {
     super.initState();
@@ -33,63 +69,161 @@ class MyBackgroundLocationState extends State<MyBackgroundLocation> {
   @override
   Widget build(BuildContext context) {
     return CustomHeader(
-      title: Text('Background Location'),
-      body: Center(
-        child: ListView(
-          children: <Widget>[
-            locationData("Latitude: " + latitude),
-            locationData("Longitude: " + longitude),
-            locationData("Accuracy: " + accuracy),
-            locationData("Speed: " + speed),
-            locationData("Time: " + time),
-            locationData("TotalDistance: " + totalDistance.toStringAsFixed(2)),
-            locationData(
-                "TotalDistance2: " + totalDistance2.toStringAsFixed(2)),
-            RaisedButton(
-                onPressed: () async {
-                  BackgroundLocation.setNotificationTitle(
-                      "Background service running");
-                  BackgroundLocation.startLocationService();
-                  BackgroundLocation.getLocationUpdates((location) {
-                    setState(() {
-                      //초기상태
+        title: Text('Relay Start'),
+        body: Center(
+            child: Container(
+                padding:
+                    EdgeInsets.only(top: 20, bottom: 5, left: 20, right: 20),
+                // color: Colors.white,
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height,
+                child: Column(children: [
+                  Expanded(
+                      flex: 40,
+                      child: FlatButton(
+                          onPressed: () {},
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                          color: royalblue,
+                          child: Container(
+                              width: double.infinity,
+                              // height: MediaQuery.of(context).size.width * 0.2,
+                              alignment: Alignment.center,
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    'Goal is 4.0 km !',
+                                    style: TextStyle(
+                                        color: lightwhite,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize:
+                                            MediaQuery.of(context).size.width /
+                                                14),
+                                    textAlign: TextAlign.center,
+                                  ),
+                                  Text(
+                                    ('Current Distance'),
+                                    style: TextStyle(
+                                        color: lightwhite,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize:
+                                            MediaQuery.of(context).size.width /
+                                                16),
+                                    textAlign: TextAlign.center,
+                                  ),
+                                  Text(
+                                    totalDistance.toStringAsFixed(1) + ' m',
+                                    style: TextStyle(
+                                        color: lightwhite,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize:
+                                            MediaQuery.of(context).size.width /
+                                                16),
+                                    textAlign: TextAlign.center,
+                                  ),
+                                  Text(
+                                    'Time Recode',
+                                    style: TextStyle(
+                                        color: lightwhite,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize:
+                                            MediaQuery.of(context).size.width /
+                                                16),
+                                    textAlign: TextAlign.center,
+                                  ),
+                                  Text(
+                                    '$_start',
+                                    style: TextStyle(
+                                        color: lightwhite,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize:
+                                            MediaQuery.of(context).size.width /
+                                                16),
+                                    textAlign: TextAlign.center,
+                                  ),
+                                  Text(
+                                    _printDuration(Duration(seconds: _start)),
+                                    style: TextStyle(
+                                        color: lightwhite,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize:
+                                            MediaQuery.of(context).size.width /
+                                                16),
+                                    textAlign: TextAlign.center,
+                                  ),
+                                ],
+                              )))),
+                  Expanded(
+                    flex: 1,
+                    child: Text(''),
+                  ),
+                  Expanded(
+                      flex: 8,
+                      child: Row(
+                        children: [
+                          Expanded(
+                              flex: 4,
+                              child: FlatButton(
+                                  onPressed: () async {
+                                    startTimer();
+                                    BackgroundLocation.setNotificationTitle(
+                                        "Background service running");
+                                    BackgroundLocation.startLocationService();
+                                    BackgroundLocation.getLocationUpdates(
+                                        (location) {
+                                      setState(() {
+                                        //초기상태
 
-                      if (beforeLat == 0 && currentLat == 1) {
-                        this.beforeLat = location.latitude;
-                        this.beforeLong = location.longitude;
-                        this.currentLat = location.latitude;
-                        this.currentLong = location.longitude;
-                      } else {
-                        this.beforeLat = double.parse(this.latitude);
-                        this.beforeLong = double.parse(this.longitude);
-                        this.currentLat = location.latitude;
-                        this.currentLong = location.longitude;
-                      }
+                                        if (beforeLat == 0 && currentLat == 1) {
+                                          this.beforeLat = location.latitude;
+                                          this.beforeLong = location.longitude;
+                                          this.currentLat = location.latitude;
+                                          this.currentLong = location.longitude;
+                                        } else {
+                                          this.beforeLat =
+                                              double.parse(this.latitude);
+                                          this.beforeLong =
+                                              double.parse(this.longitude);
+                                          this.currentLat = location.latitude;
+                                          this.currentLong = location.longitude;
+                                        }
 
-                      this.latitude = location.latitude.toString();
-                      this.longitude = location.longitude.toString();
-                      this.accuracy = location.accuracy.toString();
-                      this.altitude = location.altitude.toString();
-                      this.bearing = location.bearing.toString();
-                      this.speed = location.speed.toString();
-                      this.time = DateTime.fromMillisecondsSinceEpoch(
-                              location.time.toInt())
-                          .toString();
+                                        this.latitude =
+                                            location.latitude.toString();
+                                        this.longitude =
+                                            location.longitude.toString();
+                                        this.accuracy =
+                                            location.accuracy.toString();
+                                        this.altitude =
+                                            location.altitude.toString();
+                                        this.bearing =
+                                            location.bearing.toString();
+                                        this.speed = location.speed.toString();
+                                        this.time =
+                                            DateTime.fromMillisecondsSinceEpoch(
+                                                    location.time.toInt())
+                                                .toString();
 
-                      if (((beforeLat == 0 && currentLat == 1) == false) &&
-                          ((beforeLat == currentLat) == false)) {
-                        totalDistance += distanceInKmBetweenEarthCoordinates(
-                            beforeLat,
-                            beforeLong,
-                            currentLat,
-                            currentLong,
-                            location.speed);
+                                        if (((beforeLat == 0 &&
+                                                    currentLat == 1) ==
+                                                false) &&
+                                            ((beforeLat == currentLat) ==
+                                                false)) {
+                                          totalDistance +=
+                                              distanceInKmBetweenEarthCoordinates(
+                                                  beforeLat,
+                                                  beforeLong,
+                                                  currentLat,
+                                                  currentLong,
+                                                  location.speed);
 
-                        totalDistance2 += distance(beforeLat, beforeLong,
-                            currentLat, currentLong, 'm', location.speed);
-                      }
-                    });
-                    print("""\n
+                                          // totalDistance2 += distance(beforeLat, beforeLong,
+                                          //     currentLat, currentLong, 'm', location.speed);
+                                        }
+                                      });
+                                      print("""\n
                         beforLat: $beforeLat
                         current: $currentLat
                         Latitude:  $latitude
@@ -98,23 +232,74 @@ class MyBackgroundLocationState extends State<MyBackgroundLocation> {
                         Speed: $speed
                         Time: $time
                       """);
-                  });
-                },
-                child: Text("Start Marathon")),
-            RaisedButton(
-                onPressed: () {
-                  BackgroundLocation.stopLocationService();
-                },
-                child: Text("Stop")),
-            // RaisedButton(
-            //     onPressed: () {
-            //       getCurrentLocation();
-            //     },
-            //     child: Text("Get Current Location")),
-          ],
-        ),
-      ),
-    );
+                                    });
+                                  },
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(30),
+                                  ),
+                                  color: royalblue,
+                                  child: Container(
+                                      width: double.infinity,
+                                      // height:
+                                      //     MediaQuery.of(context).size.width *
+                                      //         0.2,
+                                      alignment: Alignment.center,
+                                      child: Column(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        children: [
+                                          Text(
+                                            'Start',
+                                            style: TextStyle(
+                                                color: lightwhite,
+                                                fontWeight: FontWeight.bold,
+                                                fontSize: MediaQuery.of(context)
+                                                        .size
+                                                        .width /
+                                                    16),
+                                            textAlign: TextAlign.center,
+                                          ),
+                                        ],
+                                      )))),
+                          Expanded(
+                              flex: 4,
+                              child: FlatButton(
+                                  onPressed: () {
+                                    isStart = false;
+                                    _timer.cancel();
+
+                                    BackgroundLocation.stopLocationService();
+                                  },
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(30),
+                                  ),
+                                  color: royalblue,
+                                  child: Container(
+                                      width: double.infinity,
+                                      // height:
+                                      //     MediaQuery.of(context).size.width *
+                                      //         0.2,
+                                      alignment: Alignment.center,
+                                      child: Column(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        children: [
+                                          Text(
+                                            'Stop',
+                                            style: TextStyle(
+                                                color: lightwhite,
+                                                fontWeight: FontWeight.bold,
+                                                fontSize: MediaQuery.of(context)
+                                                        .size
+                                                        .width /
+                                                    16),
+                                            textAlign: TextAlign.center,
+                                          ),
+                                        ],
+                                      )))),
+                        ],
+                      ))
+                ]))));
   }
 
   Widget locationData(String data) {
@@ -122,7 +307,8 @@ class MyBackgroundLocationState extends State<MyBackgroundLocation> {
       data,
       style: TextStyle(
         fontWeight: FontWeight.bold,
-        fontSize: 18,
+        fontSize: 20,
+        color: Colors.white,
       ),
       textAlign: TextAlign.center,
     );
@@ -142,7 +328,7 @@ class MyBackgroundLocationState extends State<MyBackgroundLocation> {
   }
 
   double distanceInKmBetweenEarthCoordinates(lat1, lon1, lat2, lon2, speed) {
-    if (speed > 0.8 && speed < 4) {
+    if (speed > 0.8 && speed < 6) {
       var earthRadiusKm = 6371;
       var dLat = degreesToRadians(lat2 - lat1);
       var dLon = degreesToRadians(lon2 - lon1);
@@ -159,7 +345,7 @@ class MyBackgroundLocationState extends State<MyBackgroundLocation> {
   }
 
   double distance(lat1_, lon1_, lat2_, lon2_, String unit, speed) {
-    if (speed > 0.8 && speed < 4) {
+    if (speed > 0.8 && speed < 6) {
       double lat1 = lat1_;
       double lon1 = lon1_;
       double lat2 = lat2_;
@@ -193,6 +379,7 @@ class MyBackgroundLocationState extends State<MyBackgroundLocation> {
 
   @override
   void dispose() {
+    _timer.cancel();
     BackgroundLocation.stopLocationService();
     super.dispose();
   }
