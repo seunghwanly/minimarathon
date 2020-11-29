d58ce41572f4b2d025ad7ffda16413e842199926
JungIn-Choi <wd32777@gmail.com>
2020-11-29 20:34:17 +0900

    edit member name, phone number

--- minimarathon/lib/component/body/register/edit_member_info.dart
+++ minimarathon/lib/component/body/register/edit_member_info.dart
@@ -78,6 +78,7 @@ class _EditMemberInfoState extends State<EditMemberInfo> {
       dataSnapshot.value.forEach((k, v) {
         if (k == "members") {
           var members = List<Map<dynamic, dynamic>>.from(v);
+
           members.forEach((values) {
             var eachMemberData = Map<dynamic, dynamic>.from(values);
             Member newMember = new Member();
@@ -99,16 +100,17 @@ class _EditMemberInfoState extends State<EditMemberInfo> {
         .then((DataSnapshot dataSnapshot) {
       dataSnapshot.value.forEach((k, v) {
         if (k == "members") {
-          var memberList = List<Map<dynamic, dynamic>>.from(v);
-          memberList.forEach((values) {
-            int index = memberList.indexOf(values);
+          var members = List<Map<dynamic, dynamic>>.from(v);
+          members.forEach((values) {
+            int index = members.indexOf(values);
             databaseReference
                 .child("Teams")
                 .child(widget.teamName)
                 .child("members")
                 .child(index.toString())
                 .update({
-              'name': _editingController[index].text.trim(),
+              'name': memberList[index].name,
+              'phoneNumber': memberList[index].phoneNumber,
             });
           });
         }
@@ -212,55 +214,177 @@ class _EditMemberInfoState extends State<EditMemberInfo> {
                                 borderRadius: BorderRadius.circular(30.0),
                                 color: deepPastelblue),
                             child: ListView.builder(
+                              //TODO:scrollcontroller height
+                              // physics: NeverScrollableScrollPhysics(),
+                              shrinkWrap: true,
                               itemCount: memberList.length,
                               itemBuilder: (context, index) {
-                                return new TextFormField(
-                                  enableInteractiveSelection: false,
-                                  controller: _editingController[index],
-                                  validator: (value) {
-                                    if (value.isEmpty) {
-                                      return "Please enter name";
-                                    } else
-                                      return null;
-                                  },
-                                  decoration: InputDecoration(
-                                      focusedErrorBorder: OutlineInputBorder(
-                                          borderRadius:
-                                              BorderRadius.circular(30),
-                                          borderSide: BorderSide(
-                                              color: mandarin, width: 2)),
-                                      errorBorder: OutlineInputBorder(
-                                          borderRadius:
-                                              BorderRadius.circular(30),
-                                          borderSide: BorderSide(
-                                              color: mandarin, width: 2)),
-                                      enabledBorder: OutlineInputBorder(
-                                          borderRadius:
-                                              BorderRadius.circular(30),
-                                          borderSide: BorderSide(
-                                              color: lightgrey, width: 2)),
-                                      focusedBorder: OutlineInputBorder(
-                                          borderRadius:
-                                              BorderRadius.circular(30),
-                                          borderSide: BorderSide(
-                                              color: orange, width: 2)),
-                                      errorStyle: TextStyle(
-                                          color: mandarin,
-                                          fontSize: 10.0,
-                                          fontWeight: FontWeight.w600),
-                                      hintText: memberList[index].name,
-                                      hintStyle: TextStyle(
-                                          color: lightgrey,
-                                          fontSize: 14.0,
-                                          fontWeight: FontWeight.w600),
-                                      labelText:
-                                          memberList[index].name,
-                                      labelStyle:
-                                          TextStyle(color: lightgrey, fontSize: 14.0, fontWeight: FontWeight.w600)),
-                                  textInputAction: TextInputAction.next,
-                                  keyboardType: TextInputType.numberWithOptions(
-                                      decimal: true),
-                                  focusNode: focusNameList[index],
+                                return Container(
+                                  // ------------------------ index
+                                  width:
+                                      MediaQuery.of(context).size.width * 0.7,
+                                  margin: EdgeInsets.symmetric(vertical: 10.0),
+                                  child: Column(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceEvenly,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.center,
+                                    children: [
+                                      Container(
+                                        width:
+                                            MediaQuery.of(context).size.width *
+                                                0.7,
+                                        margin:
+                                            EdgeInsets.symmetric(vertical: 5.0),
+                                        alignment: Alignment.centerLeft,
+                                        child: Text(
+                                          "Member " + (index + 1).toString(),
+                                          style: TextStyle(
+                                              color: lightwhite,
+                                              fontWeight: FontWeight.w500,
+                                              fontSize: 20.0),
+                                        ),
+                                      ),
+                                      // ------------------------ name
+                                      Container(
+                                          width: MediaQuery.of(context)
+                                                  .size
+                                                  .width *
+                                              0.7,
+                                          margin: EdgeInsets.symmetric(
+                                              vertical: 5.0),
+                                          child: TextField(
+                                            decoration: InputDecoration(
+                                              filled: true,
+                                              fillColor: Colors.transparent,
+                                              disabledBorder:
+                                                  OutlineInputBorder(
+                                                      borderRadius:
+                                                          BorderRadius.circular(
+                                                              30),
+                                                      borderSide:
+                                                          new BorderSide(
+                                                              color: mandarin,
+                                                              width: 3)),
+                                              focusedBorder: OutlineInputBorder(
+                                                  borderRadius:
+                                                      BorderRadius.circular(30),
+                                                  borderSide: BorderSide(
+                                                      color: mandarin,
+                                                      width: 3)),
+                                              enabledBorder: OutlineInputBorder(
+                                                  borderRadius:
+                                                      BorderRadius.circular(30),
+                                                  borderSide: new BorderSide(
+                                                      color: lightwhite,
+                                                      width: 3)),
+                                              hintText:
+                                                  '${memberList[index].name}',
+                                              hintStyle: TextStyle(
+                                                  color: Colors.white54),
+                                              labelText:
+                                                  '${memberList[index].name}',
+                                              labelStyle: TextStyle(
+                                                  color: Colors.white54,
+                                                  fontSize: 18,
+                                                  fontWeight: FontWeight.w500),
+                                            ),
+                                            enabled: true,
+                                            style: TextStyle(color: lightwhite),
+                                            onChanged: (name) {
+                                              setState(() {
+                                                memberList[index].name = name;
+                                              });
+                                            },
+                                            textInputAction:
+                                                TextInputAction.next,
+                                            keyboardType:
+                                                TextInputType.numberWithOptions(
+                                                    signed: true),
+                                            focusNode: focusNameList[index],
+                                            onEditingComplete: () => FocusScope
+                                                    .of(context)
+                                                .requestFocus(index == 0
+                                                    ? focusNameList[index + 1]
+                                                    : focusPhoneNumberList[
+                                                        index]),
+                                            cursorWidth: 4.0,
+                                          )),
+                                      // ------------------------ phone
+                                      Container(
+                                          width: MediaQuery.of(context)
+                                                  .size
+                                                  .width *
+                                              0.7,
+                                          margin: EdgeInsets.symmetric(
+                                              vertical: 5.0),
+                                          child: TextField(
+                                            decoration: InputDecoration(
+                                              filled: true,
+                                              fillColor: Colors.transparent,
+                                              disabledBorder:
+                                                  OutlineInputBorder(
+                                                      borderRadius:
+                                                          BorderRadius.circular(
+                                                              30),
+                                                      borderSide:
+                                                          new BorderSide(
+                                                              color: mandarin,
+                                                              width: 3)),
+                                              focusedBorder: OutlineInputBorder(
+                                                  borderRadius:
+                                                      BorderRadius.circular(30),
+                                                  borderSide: BorderSide(
+                                                      color: mandarin,
+                                                      width: 3)),
+                                              enabledBorder: OutlineInputBorder(
+                                                  borderRadius:
+                                                      BorderRadius.circular(30),
+                                                  borderSide: new BorderSide(
+                                                      color: lightwhite,
+                                                      width: 3)),
+                                              hintText:
+                                                  '${memberList[index].phoneNumber}',
+                                              hintStyle: TextStyle(
+                                                  color: Colors.white54),
+                                              labelText:
+                                                  '${memberList[index].phoneNumber}',
+                                              labelStyle: TextStyle(
+                                                  color: Colors.white54,
+                                                  fontSize: 18,
+                                                  fontWeight: FontWeight.w500),
+                                            ),
+                                            enabled: true,
+                                            style: TextStyle(color: lightwhite),
+                                            onChanged: (number) {
+                                              setState(() {
+                                                memberList[index].phoneNumber =
+                                                    '+1' + number;
+                                              });
+                                            },
+                                            textInputAction:
+                                                TextInputAction.next,
+                                            keyboardType:
+                                                TextInputType.numberWithOptions(
+                                                    signed: true),
+                                            focusNode:
+                                                focusPhoneNumberList[index],
+                                            onEditingComplete: () {
+                                              if (index == memberLength - 1) {
+                                                //last member
+                                                FocusScope.of(context)
+                                                    .requestFocus(
+                                                        focusDonationFee);
+                                              } else {
+                                                FocusScope.of(context)
+                                                    .requestFocus(focusNameList[
+                                                        index + 1]);
+                                              }
+                                            },
+                                            cursorWidth: 4.0,
+                                          ))
+                                    ],
+                                  ),
                                 );
                               },
                             ))),
